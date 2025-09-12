60c86386c4983a533406145906e2377f153ca6a5
Juan Pablo Arturo A. <juanpabloarturoazcarate2@gmail.com>
2025-09-12 17:52:25 -0500

    feat: add UsageController for managing usage consumption

---
+++ backend/src/interfaces/controllers/usage.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ConsumeUsageUseCase } from 'src/application/cosume-usage.usecase';
+import { UsagePrismaRepository } from 'src/infrastructure/prisma/repositories/usage.prisma.respository';
+import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
+
+@Controller('usage')
+export class UsageController {
+    private readonly consumeUsage: ConsumeUsageUseCase;
+
+    constructor(private readonly prisma: PrismaService) {
+        const usageRepo = new UsagePrismaRepository(prisma);
+        this.consumeUsage = new ConsumeUsageUseCase(usageRepo);
+
+    }
+    @Post('consume')
+    async consume(@Req() req: any) {
+        const userId = req.user ? req.user.id : null;
+
+        const result = await this.consumeUsage.execute(userId);
+        return result;
+
+    }
+}
